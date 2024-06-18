fb7dc16f24739b144d7fec94b0dab3d092d0cc02
fayinana <fekedeananiya@gmail.com>
2024-06-18 23:52:03 +0300

    add all section with responsive layout

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu");
+const links = document.querySelector(".links");
+
+menu.addEventListener("click", () => {
+  links.classList.toggle("hide-menu");
+});
+const dateDOM = document.querySelector('#date')
+
+const date = new Date();
+
+dateDOM.innerHTML = date.getFullYear()
